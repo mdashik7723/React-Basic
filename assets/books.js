50c05419a5f63795ff75c573c8adbc18ac38f9aa
MD ASHIKUR RAHMAN <52170700+mdashik7723@users.noreply.github.com>
2022-06-16 01:33:03 +0600

    Add files via upload

---
+++ assets/books.js
@@ -0,0 +1,6 @@
+const Books = [
+    { id: 1, bookName: "Bangladesh" , Writer: "1971"},
+    { id: 2, bookName: "Pakisthan", Writer: "1947"},
+    { id: 3, bookName: "India", Writer: "1947"}
+]
+export default Books;
